6c82a2d1115667272b11b30d1538e1e6e1a7358b
HasiniIshara <hasiniisharao4@gmail.com>
2024-02-15 21:08:33 +0200

    put comments to the Sanity Test

--- PageObjects/DashboardPage.js
+++ PageObjects/DashboardPage.js
@@ -1,16 +1,22 @@
-const { Builder, By } = require('selenium-webdriver');
-const SystemUtil = require('../Properties/SystemUtil');
+const { By } = require('selenium-webdriver');
 
+//Create Dashboard Pahge class
 class DashboardPage {
 
+    //Call constructor
     constructor(driver){
         this.driver = driver;       
     }
 
+    // Verifying Dashboard Header Text
     async verifyDashboardHeader(){
+
+        //Find dashboard page header text
         const dashboardHeader = await driver.findElement(By.xpath('//*[@id="topbar"]/ul[1]/li/div'));
+        //Get Header Title item text
         return dashboardHeader.getText();
     }
 }
 
+//Export DashboardPage class
 module.exports = DashboardPage;
--- TestCases/SanityTest.js
+++ TestCases/SanityTest.js
@@ -1,37 +1,43 @@
-const { Builder } = require('selenium-webdriver');
-const { By } = require('selenium-webdriver');
-const chrome = require('selenium-webdriver/chrome');
 const SystemUtil = require('../Properties/SystemUtil');
 const LoginPage = require('../PageObjects/LoginPage');
-const webdriver = require('selenium-webdriver');
 const users = require('../UserData/Config');
 const DashboardPage = require('../PageObjects/DashboardPage');
 const assert = require('assert');
 
 
-
+//Define variables for pages
 let systemUtil
 let loginPage;
 let dashboardPage;
 
-describe('Login Test', () => {
-    
+//This describe block is for Test the Login Page Test
+describe('Login Page Test', () => {
+
+    //This beforeEach is run before every test cases.
     beforeEach(async () =>{
+        // create page objects
         systemUtil = new SystemUtil();
-        await systemUtil.openSystem();
         loginPage = new LoginPage(systemUtil.driver);
         dashboardPage = new DashboardPage(systemUtil.driver);
+
+        //Open System
+        await systemUtil.openSystem();
+        //Login to the System
         await loginPage.successLogin(users.uName, users.uPassword);
     });
 
+    //This function is run after every test cases.
     afterEach(async () =>{
+        //Close the system.
         await SystemUtil.closeSystem();
     });
 
+    // This it block is represnt success login
     it('should log in successfully with valid credentials', async () => {
         
+        // Getting Dahsboard header text & store it to dashHeader variable.
         const dashHeader = await dashboardPage.verifyDashboardHeader();
-        assert.equal(dashHeader, 'Employee Management', 'Header text does not match the expected value.');
-        
+        //Check it's equal 'Employee Management'.
+        assert.equal(dashHeader, 'Employee Management', 'Header text does not match the expected value.');     
     });
 });
