66ac9edbffbfeb3bc9237d1126af35fc1d67cf6a
HasiniIshara <hasiniisharao4@gmail.com>
2024-02-22 18:25:57 +0200

    Changes done in the code.

--- PageObjects/AdminPage.js
+++ PageObjects/AdminPage.js
@@ -1,11 +1,13 @@
 const { By } = require('selenium-webdriver');
 
+
 //Create Admin Page Class
 class AdminPage {
 
     //Call constructor
     constructor(driver){
         this.driver = driver;
+       // const wait = new WebDriverWait(driver, 10000);
     }
 
     //Verifying Admin Module Header
@@ -16,6 +18,57 @@ class AdminPage {
         const adminHeadText = await adminHeader.getText();
         console.log(adminHeadText);
         return adminHeadText;    
+    }
+
+    //Add New Job title to the system.
+    async addNewJobTitle(){
+
+       // const wait = this.wait;
+        
+        //Find Main Job Section
+        const jobMain = await this.driver.findElement(By.xpath('//*[@id="top_level_menu_item_menu_item_102"]/a'));
+        //Click Main Job Section.
+        await jobMain.click();
+
+        //Find Manage Job Title Item.
+        const manageJobTitle = await this.driver.findElement(By.xpath('//*[@id="top_level_menu_item_menu_item_102"]/sub-menu-container/div/div[2]/a'));
+        //Click manageJobTitle
+        await manageJobTitle.click();
+
+        //Find job title Add button
+        const addJobTitleBtn = await this.driver.findElement(By.xpath('//*[@id="jobTitlesDiv"]/div/a/i'));
+        //Click Job title add button
+        await addJobTitleBtn.click();
+
+        //Find Job Title text field.
+        const jobTitleTxt = await this.driver.findElement(By.id('jobTitleName'));
+        //Enter Value
+        await jobTitleTxt.sendKeys('HasQA1');
+
+        //Find Job Description text field.
+        const jobDescTxt = await this.driver.findElement(By.id('jobDescription'));
+        //Enter Value
+        await jobDescTxt.sendKeys('Test Job Title');
+
+        //Find Save button
+        const jobSaveBtn = await this.driver.findElement(By.id('modal-save-button'));
+        //Click save button
+        await jobSaveBtn.click();
+
+        // try {
+        //     const successMessageDisplayed = await wait.until(async (driver) => {
+        //         const toastMessage = await this.driver.findElement(By.css('.toast-message'));
+        //         return await toastMessage.isDisplayed();
+        //     });
+        //     console.log(successMessageDisplayed);
+        //     assert.strictEqual(successMessageDisplayed, true, "Job Title is not added successfully");
+        //     console.log("Successfully added new job title");
+        // } catch (error) {
+        //     // If adding fails, then validation should appear & toast message should not
+        //     const errorMessageDisplayed = await driver.findElements(By.xpath("//*[@id='modal-holder']/div/div/div/div[2]/form/oxd-decorator/div/div[1]/div[2]/span"));
+        //     assert.strictEqual(errorMessageDisplayed.length > 0, true, "Validation error message not displayed");
+        //     console.log("Duplicate Data Entry");
+        // }
     }
 }
 
--- TestCases/SanityTest.js
+++ TestCases/SanityTest.js
@@ -37,7 +37,7 @@ describe('Sanity Test', () => {
 
     //First describe block. This is the Login Functionality.
     describe('Login to the System', () => {
-        //First Test Case inside this describe block.
+        //Checking the Successful login.
         it('Should log in successfully with valid credentials', async () => {
             //Getting Dashboard Header title from DashboardPage.js file.
             const dashHeader = await dashboardPage.verifyDashboardHeader();
@@ -48,13 +48,22 @@ describe('Sanity Test', () => {
 
     //Second describe block. This is for Admin Module Test Cases.
     describe('Login to the Admin Module', () => {
+        //Checking successful login to the Admin Module.
         it('Should log in successfully to Admin Module', async () => {
-            //Click Side Bar Admin Module
+            //Click Side Bar Admin Module.
             await dashboardPage.clickAdminModule();
             //Getting Admin Module Bpard Header title from AdminPage.js file.
             const adminModhHeader = await adminPage.verifyingAdminModuleHeader();
             //Compare the title with Actual value.
             assert.equal(adminModhHeader, 'HR Administration', 'Header text does not match the expected value.');
+        });
+
+        //Checking Add new job title to the system.
+        it('Should adding new job title in successfullt to the Job section', async () =>{
+            //Click Side Bar Admin Module.
+            await dashboardPage.clickAdminModule();
+            //Add New Job to the system.
+            await adminPage.addNewJobTitle();
         });
     });
 });
